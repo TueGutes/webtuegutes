4cab5f4602b8bb82b9f9970f74c073d183a82b4f
nolting <nick.nolting@stud.hs-hannover.de>
2016-10-11 14:17:54 +0200

    Datenbankpfade überarbeitet

--- verbinde.php
+++ verbinde.php
@@ -4,7 +4,7 @@
 DEFINE('DB_USER','root');
 DEFINE('DB_PASSWORD','');
 DEFINE('DB_HOST','localhost');
-DEFINE('DB_NAME','testDatenbank');
+DEFINE('DB_NAME','tuegutesdb');
 $db = mysqli_connect(DB_HOST,DB_USER, DB_PASSWORD, DB_NAME);
 if(!$db)
 {
@@ -18,7 +18,8 @@ $benutzername = $_POST['benutzername'];
 $passwort = $_POST['passwort'];
 $mail_php = $_POST['mail'];
 #$users_comment = $_POST['comment'];
-$stmt_text = "INSERT INTO tuegutesdb (Benutzername, Email, Passwort, RegDatum) values('$benutzername','$passwort','$mail_php',CURDATE())";
+$datum = CURDATE();
+$stmt_text = "INSERT INTO Benutzer (Benutzername, Email, Passwort, RegDatum) values('$benutzername','" . sha1($passwort.$date) . ") ','" . "$mail_php" . "'," . $datum . ")";
 #echo $stmt_text;
 
 $stmt = mysqli_prepare($db,$stmt_text) or die(mysqli_error($db));
