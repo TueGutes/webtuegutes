9128f47e16d00861183a7397a24e6743ceae4855
nolting <nick.nolting@stud.hs-hannover.de>
2016-10-17 10:02:19 +0200

    Kurze provisorische Möglichkeit eingefügt, von der Kontoerstellung durch einen Klick auf ein ebenfalls provisorisches Logo wieder auf die Startseite zu gelangen

--- top.php
+++ top.php
@@ -1,7 +1,14 @@
 <div style="margin-left:25%;margin-right:25%;background-color:#757575">
-	<div style="margin-left:53%;text-align:left">
-		<iframe src="login.php" frameBorder="0">
-			include "login.php";
-		</iframe>
-	</div>
+	<table width="100%">
+		<tr>
+			<td style="width:25%" align="center">
+				<a href="/index.php"><img src="img/logo_provisorisch.png" alt="Zurück zur Startseite" title="Zurück zur Startseite" style="width:75%"/></a>
+			</td>
+			<td style="width:75%" align=right>
+				<iframe src="login.php" frameBorder="0">
+					include "login.php";
+				</iframe>
+			</td>
+		</tr>
+	</table>
 </div>
