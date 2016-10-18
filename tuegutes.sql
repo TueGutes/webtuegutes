ab8caccaf030449d318f06d32886d9080a6d9e6b
nolting <nick.nolting@stud.hs-hannover.de>
2016-10-18 12:27:16 +0200

    Neue Datenbank eingefügt

--- tuegutes.sql
+++ tuegutes.sql
@@ -1,12 +1,24 @@
 drop table Benutzer;
 
 create table Benutzer(
-UserID int NOT NULL AUTO_INCREMENT,
-Benutzername varchar(48) NOT NULL,
-Passwort varchar(32) NOT NULL,
-Email varchar(128) NOT NULL,
-RegDatum varchar(32) NOT NULL,
-UNIQUE (Benutzername),
-UNIQUE (Email),
-PRIMARY KEY (UserID)
+	UserID int NOT NULL AUTO_INCREMENT,
+	Benutzername varchar(48) NOT NULL,
+	Passwort varchar(32) NOT NULL,
+	Email varchar(128) NOT NULL,
+	RegDatum varchar(32) NOT NULL,
+	UNIQUE (Benutzername),
+	UNIQUE (Email),
+	Vorname varchar(64) NOT NULL,
+	Nachname varchar(64) NOT NULL,
+	Geburtsjahr int,
+	Geburtstag varchar(4),
+	Avatar text,
+	Strasse varchar(128),
+	Hausnummer varchar(5),
+	TelNr varchar(20),
+	MsgNr varchar(20),
+	Hobbys tinyText,
+	Beschreibung text,
+	Datenschutz varchar(13),
+	PRIMARY KEY (UserID)
 );
