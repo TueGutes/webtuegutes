c26d1d8cafb0e8f1798f3ebea8c92aa6e4390f4f
nolting <nick.nolting@stud.hs-hannover.de>
2016-10-18 09:55:02 +0200

    Minor text fixes

--- top.php
+++ top.php
@@ -1,11 +1,11 @@
 <div style="margin-left:25%;margin-right:25%;background-color:#757575">
 	<table width="100%">
 		<tr>
-			<td style="width:25%" align="center">
+			<td width="25%" align="center">
 				<a href="./index.php"><img src="img/logo_provisorisch.png" alt="Zurück zur Startseite" title="Zurück zur Startseite" style="width:75%"/></a>
 			</td>
-			<td style="width:75%" align=right>
-				<iframe src="login.php" width="45%" frameBorder="0">
+			<td align="right">
+				<iframe src="login.php" frameBorder="0">
 					include "login.php";
 				</iframe>
 			</td>
