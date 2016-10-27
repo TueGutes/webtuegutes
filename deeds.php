2e2f9a884d46800e121228d0487ef20e796db2db
Lukas Buttke <lukas.buttke@stud.hs-hannover.de>
2016-10-27 15:22:43 +0200

    Anpassen der deeds

--- deeds.php
+++ deeds.php
@@ -11,11 +11,22 @@ require './includes/_top.php';
 
 <html>
 <head> <title> Gute Taten anzeigen </title> 
+<script>
+var nextPage = function(){
+	document.getElementById("out").innerHTML = "Button gedruckt";
+}
 
+</script> 
 </head>
 
 <body> 
 <h2><?php echo $wlang['deeds_head']; ?> </h2>
+
+<!-- Hier kann später mal gute Taten erstellen hervorkommen-->
+<div class='ctop'>
+<input type="button" value="Gute Tat erstellen" onclick="window.open('./db/erstelleDeed.php','Deed')">
+<br> <hr>
+</div>
 
 <div class='center'>
 	<table>
@@ -23,24 +34,28 @@ require './includes/_top.php';
 		$mysqli = db_connect();
 		$result = $mysqli->query("SELECT * FROM deeds");
 		// Tabellenkopf mit den Feldnamen als Spaltenbezeichnungen:
-		echo " <tr>\n";
+		echo " <tr>";
 		while ( $field = $result->fetch_field() ) {
 			if(($field->name !== "idGuteTat")&&($field->name !== "contactPerson")&&($field->name !== "status")){
-				echo " <th>$field->name</th>\n";
+				echo " <th>$field->name</th>";
 			}
 		}
-		echo " </tr>\n";
+		
+// 		Versuch einen Button mit in die Tabelle zu stellen misglückt.
+//		<input type="button" value="weiterlesen" onclick="nextPage();"> 
+//		<p id="out"> yo </p>
+		 
+		echo " </tr>";
 		// Tabelleneinträge aus der Datenbank
 		while ( $deed = $result->fetch_object() ) {
 
-			echo " <tr>\n";
+			echo " <tr>";
 			foreach ( $deed as $key => $value ) 
 			{
-					if($key !== "idGuteTat"){
-						echo " <td>$value</td> \n";
-					}	
+				echo " <td>$value</td> ";	
 			}
-			echo " </tr>\n";
+
+			echo " </tr>";
 		}
 		?>
 	</table>
