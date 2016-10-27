9b5a33b5587f2e5d16e85be9e2f1e55603c29011
wesone <mail@wesone.de>
2016-10-27 10:05:12 +0200

    temporäre css Dateien

--- deeds.php
+++ deeds.php
@@ -3,14 +3,26 @@
 *@author Henrik Huckauf
 */
 
-include './includes/ACCESS.php';
+//include './includes/ACCESS.php';
 
 require './includes/_top.php';
 ?>
 
-<h2><?php echo $wlang['login_head']; ?></h2>
+<h2><?php echo $wlang['deeds_head']; ?></h2>
 
-Blablabla
+<div class='center'>
+	<table>
+		<tr>
+			<td>cndj</td>
+			<td>cndj</td>
+			<td>cndj</td>
+			<td>cndj</td>
+			<td>cndj</td>
+			<td>cndj</td>
+			<td>cndj</td>
+		</tr>
+	</table>
+</div>
 
 <?php
 require './includes/_bottom.php';
