d9abc147ad6c24aadf7207f461d42233f5a1e555
Lukas Buttke <lukas.buttke@stud.hs-hannover.de>
2016-10-27 14:11:15 +0200

    Fehler in deeds ausbessern

--- deeds.php
+++ deeds.php
@@ -1,24 +1,53 @@
 <?php
 /*
-*@author Henrik Huckauf
+*@author Lukas Buttke
 */
 
 //include './includes/ACCESS.php';
+
 include './db/db_connector.php';
 require './includes/_top.php';
 ?>
 
+<html>
+<head> <title> Gute Taten anzeigen </title> 
+
+</head>
+
+<body> 
 <h2><?php echo $wlang['deeds_head']; ?> </h2>
+
 <div class='center'>
 	<table>
 		<?php
 		$mysqli = db_connect();
 		$result = $mysqli->query("SELECT * FROM deeds");
 		// Tabellenkopf mit den Feldnamen als Spaltenbezeichnungen:
-		echo " <tr>\n";asdsd
+		echo " <tr>\n";
+		while ( $field = $result->fetch_field() ) {
+			if(($field->name !== "idGuteTat")&&($field->name !== "contactPerson")&&($field->name !== "status")){
+				echo " <th>$field->name</th>\n";
+			}
+		}
+		echo " </tr>\n";
+		// Tabelleneinträge aus der Datenbank
+		while ( $deed = $result->fetch_object() ) {
+
+			echo " <tr>\n";
+			foreach ( $deed as $key => $value ) 
+			{
+					if($key !== "idGuteTat"){
+						echo " <td>$value</td> \n";
+					}	
+			}
+			echo " </tr>\n";
+		}
 		?>
-	</tableasdsad
+	</table>
 </div>
+
+</body>
+</html>
 
 <?php
 require './includes/_bottom.php';
