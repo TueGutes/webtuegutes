0a9e068357c6133d54dbdcc46a387582b19337f9
Shanghui Dai <shanghui.dai@stud.hs-hannover.de>
2016-10-31 20:11:28 +0100

    +search_demo

--- search.php
+++ search.php
@@ -1,20 +1,24 @@
 <?php
-include "function.php";
-    $db=new db();
-    $db->connect();
+// demo, es gibt noch Problem
+require './includes/_top.php';
+require './db_connector.php';
 
+$name= ($_GET['key']);
+$db = db_connect();
 
-    if ($_GET[key]) {
-        $key = explode('',$_GET[key]);
-        $sql = "SELECT * FROM ` ` where  like '%$k[0]%' or name like '%$k[1]'";
-        echo '<br>';
-        $query = $db->query($sql);
-        while ($r = $db->fetch_array($query)) {
-            $r[xxx] = preg_replace("/($k[0])/i", '<font color=red><b>\\1</b></font>', $r[name]);
-            $r[xxx] = preg_replace("/($k[1])/i", '<font color=red><b>\\1</b></font>', $r[name]);
-            echo $r[name]."<br>";
-        }
+if ($name){
+    $key = explode(" ",$name);
+    $sql = "SELECT * FROM `Deeds` where  like '%$k[0]%' or name like '%$k[1]' or category like '%$k[0]%' or category like '%$k[1]'";
+    $result = $db->query($sql);
+    db_close($db);
+    $arr = array();
+    while($dbentry =$result->fetch_object()){
+        $arr[]= $dbentry();
     }
+    return $arr;
+}
+
+
  ?>
 
  <form action="" method="GET">
