3bef78169c469f9682fb616fc7e811f146f4eeed
Shanghui Dai <shanghui.dai@stud.hs-hannover.de>
2016-10-31 16:56:10 +0100

    add modify

--- search.php
+++ search.php
@@ -1,21 +1,21 @@
 <?php
-include "objektorientiertes_beispiel/db.php";
-    $db=new DB();
-    db->connect("localhost","","root","");
+include "function.php";
+    $db=new db();
+    $db->connect();
 
 
-    if ($_GET[key] {
-        $key = explode(' ', $_GET[key]);
+    if ($_GET[key]) {
+        $key = explode('',$_GET[key]);
         $sql = "SELECT * FROM ` ` where  like '%$k[0]%' or name like '%$k[1]'";
         echo '<br>';
-        $q = $db->query($sql);
-        while ($r = $db->fetch_array($q)) {
+        $query = $db->query($sql);
+        while ($r = $db->fetch_array($query)) {
             $r[xxx] = preg_replace("/($k[0])/i", '<font color=red><b>\\1</b></font>', $r[name]);
             $r[xxx] = preg_replace("/($k[1])/i", '<font color=red><b>\\1</b></font>', $r[name]);
+            echo $r[name]."<br>";
         }
     }
  ?>
-
 
  <form action="" method="GET">
     Stichwort:
