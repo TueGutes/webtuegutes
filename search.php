04d247ad2be69d67524efaa32d35fe1e6fdd2a6e
Shanghui Dai <shanghui.dai@stud.hs-hannover.de>
2016-10-31 20:19:16 +0100

    search_demo

--- search.php
+++ search.php
@@ -3,21 +3,21 @@
 require './includes/_top.php';
 require './db_connector.php';
 
-$name= ($_GET['key']);
+$name = ($_GET['key']);
 $db = db_connect();
 
-if ($name){
-    $key = explode(" ",$name);
-    $sql = "SELECT * FROM `Deeds` where  like '%$k[0]%' or name like '%$k[1]' or category like '%$k[0]%' or category like '%$k[1]'";
+if ($name) {
+    $key = explode(' ', $name);
+    $sql = "SELECT * FROM `Deeds` where  like '%$key[0]$key[1]%' or name like '%$key[1]$key[2]%' or category like '%$key[0]$key[1]%' or category like '%$key[1]$key[2]%'";
     $result = $db->query($sql);
     db_close($db);
     $arr = array();
-    while($dbentry =$result->fetch_object()){
-        $arr[]= $dbentry();
+    while ($dbentry = $result->fetch_object()) {
+        $arr[] = $dbentry();
     }
+
     return $arr;
 }
-
 
  ?>
 
