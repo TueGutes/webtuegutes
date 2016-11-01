9b8afeb288387a8f1cc4e9a10b22818216e2b060
Shanghui Dai <shanghui.dai@stud.hs-hannover.de>
2016-11-01 15:43:15 +0100

    search_function_demo2

--- search.php
+++ search.php
@@ -1,28 +1,30 @@
 <?php
-// demo, es gibt noch Problem
+session_start();
+include './includes/db_connector.php';
 require './includes/_top.php';
-require './db_connector.php';
+error_reporting(0);
 
-$name = ($_GET['key']);
+// demo, es gibt noch Problem
+?>
+<form action="" method="get">
+   Stichwort:
+    <input type="text" name="st" >
+    <input type="submit" name="sub" value="search">
+</form>
+<?php
 $db = db_connect();
-
-if ($name) {
-    $key = explode(' ', $name);
-    $sql = "SELECT * FROM `Deeds` where  like '%$key[0]$key[1]%' or name like '%$key[1]$key[2]%' or category like '%$key[0]$key[1]%' or category like '%$key[1]$key[2]%'";
-    $result = $db->query($sql);
-    db_close($db);
-    $arr = array();
-    while ($dbentry = $result->fetch_object()) {
-        $arr[] = $dbentry();
+if ($_GET['st']) {
+    $k = explode(' ',$_GET['st']);
+    $sql = "SELECT * FROM `Deeds` where name like '%$k[0]$k[1]%' or category like '%$k[0]$k[1]%'";
+    $result = mysqli_query($db, $sql);
+    echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+    while ($row=mysqli_fetch_object($result)) {
+        echo "Gute Tat:$row->name,&nbsp;Kategorie:$row->category,&nbsp;Status:$row->status";
     }
-
-    return $arr;
+    db_close($db);
 }
+?>
 
+ <?php
+ require './includes/_bottom.php';
  ?>
-
- <form action="" method="GET">
-    Stichwort:
-     <input type="text" name="key" >
-     <input type="submit" name="sub" value="search">
- </form>
