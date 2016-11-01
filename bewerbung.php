9ebe1bf50eebf2138ad12ef1261955fa86b905b8
blecha <andreas.blech@stud.hs-hannover.de>
2016-11-01 20:46:40 +0100

    Weitere Fehlermeldungen eingebunden

--- bewerbung.php
+++ bewerbung.php
@@ -29,7 +29,7 @@ DB Funktionen:
 
 	statusOfBewerbung($userID, $tatID) //Oder false, falls keine Bewerbung vorliegt
 	acceptBewerbung($userID, $tatID, $explanation)
-
+	declineBewerbung($userID, $tatID, $explanation)
 */
 
 
@@ -43,13 +43,18 @@ Es gibt ... Fälle:
 		1.4 Die gute Tat wurde bereits abgeschlossen (statusOfGuteTatById())
 		1.5 Die Anzahl der angenommenen Bewerbungen ist gleich der Anzahl der benötigten Helfer (isNumberOfAcceptedCandidatsEqualToRequestedHelpers()) : Hinweis: Man ist nur Reserve, aber bewerben kann man sich trotzdem
 		1.6 alles in Ordnung, der Nutzer kann sich mit einem Bewerbungstext bewerben
-			  und seine Bewerbung abschicken, danach wird er auf die Detailseite der Tat weitergeleitet. Es wird eine Email an den Ersteller der Guten Tat gesendet mit dem Link zur Bewerbung (addBewerbung())
+			  und seine Bewerbung abschicken. 
 	2. Ein Nutzer schaut sich die Bewerbung eines anderen Benutzers an isset $_GET['tatID'] && $_GET['userID']
 		1.1 Der Benutzer hat die Tat nicht erstellt und darf keine Bewerbungen annehmen (contactPersonOfGuteTatById())
 		1.2 Die Bewerbung wurde bereits akzeptiert (statusOfBewerbung())
-		1.3 Es gibt keine Bewerbung von diesem Nutzer zu dieser Tat
-		1.4 alles in Ordnung, der Nutzer kann die Bewerbung mit einem Text annehmen oder ablehnen, wird auf die Detailseite weitergeleitet und der Bewerber bekommt eine Email mit dem Link zur Detailseite der guten Tat (acceptBewerbung())
-		1.* Anzahl der Helfer zu groß, um sie anzunehmen	
+		1.3 Die Bewerbung wurde bereits abgelehnt (status Of Bewerbung())
+		1.4 Es gibt keine Bewerbung von diesem Nutzer zu dieser Tat
+		1.5 Anzahl der Helfer zu groß, um sie anzunehmen	
+		1.6 alles in Ordnung, der Nutzer kann die Bewerbung mit einem Text annehmen oder ablehnen
+	3. Fall: Bewerbungsformular wurde abgeschickt.  Es wird eine Email an den Ersteller der Guten Tat gesendet mit dem Link zur Bewerbung (addBewerbung()). Es wird eine Bestätigung angezeigt und ein Link zur Detailseite der guten Tat. Außerdem wird ein Eintrag in der Datenbank vorgenommen
+	4. Fall: Bewerbung-Annahme Formular wurde abgeschickt. Der Bewerber bekommt eine Anname-Email mit dem Link zur Detailseite der guten Tat 	(acceptBewerbung()) der Datenbankeintrag wird angepasst, ein neuer Eintrag in der Datenbank für die Helfer vorgenommen und eine Bestätigung + Plus Link zur Detailseite der Guten Tat wird angezeigt
+	5. Fall: Bewerbung-Absage Formular wurde abgeschickt. Der Bewerber bekommt eine Absage-Email mit dem Link zur Detailseite der guten Tat
+	(declineBewerbung()) der Datenbankeintrag wird angepasst und eine Bestätigung + LInk zur Detailseite der Guten Tat wird angezeigt
 */
 //Fall 0: Profile sind nur für eingeloggte Nutzer sichtbar:
 if (!@$_SESSION['loggedIn']) die ('<h3>Bewerbungen sind nur für eingeloggte Nutzer sichtbar!</h3><p/><a href="login.php">Zum Login</a>');
@@ -83,7 +88,7 @@ if(isset($_GET['idGuteTat']) && !isset($_GET['candidateID'])) {
 			//Fall 1.5: Die Anzahl der angenommenen Bewerbungen erfüllt die Anzahl der gewünschten Helfer
 			echo '<h4>Hinweis: Diese gute Tat hat bereits genügend Helfer, du wirst u.U. als Reserve eingesetzt</h4>';
 		}
-		//Fall 1.6 (und 1.5): Nutzer kann Bewerbung abschicken und wird anschließend auf Detailseite der Guten Tat weitergeleitet
+		//Fall 1.6 (und 1.5): Nutzer darf die Bewerbung inkl. eines Bewerbungstextes abschicken
 		
 	}
 	
@@ -93,15 +98,40 @@ elseif(isset($_GET['idGuteTat']) && isset($_GET['candidateID'])) {
 	$idGuteTat = $_GET['idGuteTat'];
 	$candidateID = $_GET['candidateID'];
 	$userID = $_SESSION['user'];
+	$status = statusOfBewerbung($userID, $tatID);
 	if(contactPersonOfGuteTatById($idGuteTat) != $userID) {
 		//Fall 1.1: Der Nutzer hat die gute Tat nicht erstellt und darf dementsprechend ihre Bewerbungen nicht annehmen
 		echo '<h3><red>Du darfst nur Bewerbungen zu guten Taten einsehen, die du selbst erstellt hat</red></h3>'; 
 	}
-	elseif() {
-		
+	elseif($status === "angenommen") {
+		//Fall 1.2: Die Bewerbung wurde bereits angenommen
+		echo '<h3><red>Die Bewerbung wurde bereits angenommen - der Bewerber wurde informiert</red></h3>'; 
+	}
+	elseif($status === "abgelehnt") {
+		//Fall 1.3: Die Bewerbung wurde bereits abgelehnt
+		echo '<h3><red>Die Bewerbung wurde bereits abgelehnt - der Bewerber wurde informiert</red></h3>'; 
+	}
+	elseif($status === false) {
+		//Fall 1.4: Es existiert keine Bewerbung dieses Nutzers für die gute Tat (sollte Websitetechnisch niemals passieren)
+		echo '<h3><red>Es existiert keine Bewerbung des Bewerbers für die Tat</red></h3>'; 
+	}
+	elseif(isNumberOfAcceptedCandidatsEqualToRequestedHelpers() === true) {
+		//Fall 1.5: Anzahl der angeforderten Helfer bereits erreicht, Bewerbung kann nicht angenommen werden, bleibt ausstehend
+		echo '<h3><red>Die Anzahl der angeforderten Helfer für diese Tat wurde bereits erreicht. </p> Die Bewerbung muss u.U. später akzeptiert werden, falls ein Helfer absagt.</red></h3>'; 
+	}
+	else {
+		//Fall 1.6: Bewerbung ist okay und kann angenommen oder abgelehnt werden inkl. einer Begründung.
 		
 	}
+}
+elseif() {
+	//Fall 3: Bewerbungsformular wurde abgeschickt
+}
+elseif() {
+	//Fall 4: 
 	
+}
+elseif() {
 	
 }
 else {
