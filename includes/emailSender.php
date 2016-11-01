d40ef01005b5d418f87d9c844ffb197b677b2ce0
blecha <andreas.blech@stud.hs-hannover.de>
2016-11-01 13:54:31 +0100

    Es gibt die Möglichkeit über eine globale Variable zu bestimmen auf welche Weise die Mails versendet werden

--- includes/emailSender.php
+++ includes/emailSender.php
@@ -1,33 +1,45 @@
 <?php 
 
+include './includes/DEF.php';
+
 /*Sends an email from tuegutesinhannover@gmail.com to $to with subject $subject and
 HTML Content $content*/
 function sendEmail($to, $subject, $content) {
-	require 'PHPMailer-master/PHPMailerAutoload.php';
-	$phpmail = new PHPMailer;
-	$phpmail->isSMTP();
-	$phpmail->SMTPSecure = 'ssl';
-	$phpmail->SMTPAuth = true;
-	$phpmail->Host = 'smtp.gmail.com';
-	$phpmail->Port = 465;
-	$phpmail->Username = 'tuegutesinhannover@gmail.com';
-	$phpmail->Password = 'TueGutes1234';
-	//$mail->setFrom('Tue Gutes in Hannover');
-	$phpmail->setFrom('tuegutesinhannover@gmail.com');
-	$phpmail->addAddress($to);
-	//$mail->addAddress('Andreas.blech@t-online.de');
-	$phpmail->Subject = $subject;
-	$phpmail->msgHTML($content);
-	$phpmail->AltBody = 'This is a plain-text message body'; 						
-	//send the message, check for errors
-	if (!$phpmail->send()) {
- 		//echo "ERROR: Sending Mail " . $phpmail->ErrorInfo;
- 		return false;
+	if($USE_GMAIL === true) {
+		/*Nutze den Gmail-Account, um die Mails zu senden*/
+		require 'PHPMailer-master/PHPMailerAutoload.php';
+		$phpmail = new PHPMailer;
+		$phpmail->isSMTP();
+		$phpmail->SMTPSecure = 'ssl';
+		$phpmail->SMTPAuth = true;
+		$phpmail->Host = 'smtp.gmail.com';
+		$phpmail->Port = 465;
+		$phpmail->Username = 'tuegutesinhannover@gmail.com';
+		$phpmail->Password = 'TueGutes1234';
+		//$mail->setFrom('Tue Gutes in Hannover');
+		$phpmail->setFrom('tuegutesinhannover@gmail.com');
+		$phpmail->addAddress($to);
+		//$mail->addAddress('Andreas.blech@t-online.de');
+		$phpmail->Subject = $subject;
+		$phpmail->msgHTML($content);
+		$phpmail->AltBody = 'This is a plain-text message body'; 						
+		//send the message, check for errors
+		if (!$phpmail->send()) {
+ 			//echo "ERROR: Sending Mail " . $phpmail->ErrorInfo;
+ 			return false;
+		}
+		else {
+			return true;
+		}	
 	}
 	else {
-		return true;
-	}	
-	
+		/*Nutze die PHP mail funktion */
+		//$to      = 'daniel.kadenbach@hs-hannover.de';
+		//$subject = 'the subject';
+		//$message = 'hello';
+		$headers = 'From: proanvil@hs-hannover.de' . "\r\n" .'X-Mailer: PHP/' . phpversion();
+		mail($to, $subject, $content, $headers);
+	}
 	/*"<div style=\"margin-left:10%;margin-right:10%;background-color:#757575\"><img src=\"img/logo_provisorisch.png\" alt=\"Zurück zur Startseite\" title=\"Zurück zur Startseite\" style=\"width:25%\"/></div><div style=\"margin-left:10%;margin-right:10%\"><h1>Herzlich Willkommen <b>".$vorname."</b> bei 'Tue Gutes in Hannover':</h1> <h3>Klicke auf den Link, um deine Registrierung abzuschließen: http://localhost/git/registration.php?e=".base64_encode($user)." </h3></div>"*/
 	
 }
