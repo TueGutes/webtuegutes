fb99bddcc548a6f9549f0525300821f84de539ba
nolting <nick.nolting@stud.hs-hannover.de>
2016-11-01 10:19:09 +0100

    Standardort auf Hannover gesetzt

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@
 		$stmt->execute();
 		$result = $stmt->get_result();
 		if (!isset($result->fetch_assoc['postalcode'])) {
-			$sql = 'INSERT INTO Postalcode (postalcode, place) VALUES (?, "Unbekannt")';
+			$sql = 'INSERT INTO Postalcode (postalcode, place) VALUES (?, "Hannover")';
 			$stmt = $db->prepare($sql);
 			$stmt->bind_param('i',$plz);
 			$stmt->execute();
