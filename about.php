8714d7c2d1ff2a1ea0c07a45266a193eaf4e8eb3
agauggel <alexander.gauggel@stud.hs-hannover.de>
2016-11-01 11:12:13 +0100

    Ueber uns-Text hinzugefuegt

--- about.php
+++ about.php
@@ -9,7 +9,11 @@ require './includes/_top.php';
 
 <h2>Über uns...</h2>
 
-Über uns...
+Diese Website entsteht im Rahmen des Bachelorprojektes des 5. und 6. Semesters des Studiengangs "Angewandte Informatik" an der Hochschule Hannover.
+
+Produktverantwortlicher: Daniel Kadenbach
+
+Team: Andreas Blech, Lukas Buttke, Shanghui Dai, Alexander Gauggel, Christian Hock, Henrik Huckauf, Max Lindemann, Nick Nolting, Timm Romanik, Klaus Sobotta, Florian Sosch
 
 <?php
 require './includes/_bottom.php';
