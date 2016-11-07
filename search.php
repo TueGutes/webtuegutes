ac5864dfe0ba40956204e85713b1d353c0294b61
Shanghui Dai <shanghui.dai@stud.hs-hannover.de>
2016-11-07 13:28:49 +0100

    basis_search

--- search.php
+++ search.php
@@ -7,24 +7,41 @@ error_reporting(0);
 // demo, es gibt noch Problem
 ?>
 <form action="" method="get">
-   Stichwort:
-    <input type="text" name="st" >
+    Stichwort:
+    <input type="text" name="st">
     <input type="submit" name="sub" value="search">
 </form>
 <?php
-$db = db_connect();
+//$db = db_connect();
+$db = mysqli_connect('localhost','tueGutes','Sadi23n2os','tueGutes');
 if ($_GET['st']) {
-    $k = explode(' ',$_GET['st']);
+    $k = explode(' ', $_GET['st']);
     $sql = "SELECT * FROM `Deeds` where name like '%$k[0]$k[1]%' or category like '%$k[0]$k[1]%'";
-    $result = mysqli_query($db, $sql);
-    echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
-    while ($row=mysqli_fetch_object($result)) {
-        echo "Gute Tat:$row->name,&nbsp;Kategorie:$row->category,&nbsp;Status:$row->status";
-    }
-    db_close($db);
+    $result = mysqli_query($db, $sql); ?>
+    <br><br><br><br><br><br><br><br><br>
+    <table style="line-height:40px;text-align:center;width:100%;font-size:20px;border:1px solid gray;clear:both">
+        <caption><h1>Result</h1></caption>
+        <tr style="font-size: 30px">
+            <th>Gute Tat</th>
+            <th>Kategorie</th>
+            <th>Status</th>
+        </tr>
+        <tbody>
+        <?php
+        while ($row = mysqli_fetch_object($result)) {
+            ?>
+            <tr>
+                <td><?php echo $row->name; ?></td>
+                <td><?php echo $row->category; ?></td>
+                <td><?php echo $row->status; ?></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    <?php mysqli_close($db);
 }
 ?>
-
- <?php
- require './includes/_bottom.php';
- ?>
+<!--        echo "Gute Tat:$row->name,&nbsp;Kategorie:$row->category,&nbsp;Status:$row->status";-->
+<?php
+require './includes/_bottom.php';
+?>
