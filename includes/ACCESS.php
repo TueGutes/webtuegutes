b35c0fc80ac38f462da8151d4ac2982e03a69452
wesone <mail@wesone.de>
2016-11-11 15:54:56 +0100

    path bug gefixt

--- includes/ACCESS.php
+++ includes/ACCESS.php
@@ -3,13 +3,13 @@
 *@author Henrik Huckauf
 */
 
-$path = dirname($_SERVER['PHP_SELF']);
+//$path = dirname($_SERVER['PHP_SELF']);
 
 if(!$_USER->loggedIn())
 {
-	$continue = $HOST . '' . $_SERVER['REQUEST_URI'];
-	$to = 'login?code=423&continue=' . urlencode($continue);
-	$_USER->redirect($HOST . ($path == '/' ? '' : $path) . $to);
+	$continue = $HOST;
+	$to = '/login?code=423&continue=' . urlencode($continue);
+	$_USER->redirect($HOST /*. ($path == '/' ? '' : $path)*/ . $to);
 	exit;
 }
 ?>
