68643fd749206f740459f7c2237a856394565305
wesone <mail@wesone.de>
2016-11-11 10:31:34 +0100

    PrivacyHandler ist nun Teil der UTILS

--- includes/UTILS.php
+++ includes/UTILS.php
@@ -3,6 +3,10 @@
 *@author Henrik Huckauf
 */
 
+/*
+* DateHandler
+*@author Henrik Huckauf
+*/
 class DateHandler
 {
 	/*private $year = 0;
@@ -121,5 +125,39 @@ class DateHandler
 		$this->date->setTime($this->getHours(), $this->getMinutes(), $w);
 		return $this;
 	}
-}	
+}
+
+/*
+* PrivacyHandler
+*@author Henrik Huckauf
+*/
+class PrivacyHandler
+{
+	private $privacyFlags;
+	/*
+	private $var = 1;
+    private static $staticVar = 2;
+    $this->var;
+    self::$staticVar;
+	*/
+	public static $NAME = 0;
+	public static $EMAIL = 1;
+	public static $BIRTHDAY = 2;
+	public static $BIRTHDAY_YEAR = 3;
+	public static $DESCRIPTION = 4;
+
+	function __construct($privacyString)
+	{
+		$this->setPrivacyString($privacyString);
+	}
+	public function setPrivacyString($privacyString)
+	{
+		$this->$privacyFlags = str_split($privacyString); // explode benötigt zwingend delimiter (!= '')
+	} 
+	// $ph->get($ph::EMAIL);
+	public function get($key)
+	{
+		return $privacyFlags[$key] === "1" ? true : false;
+	}
+}
 ?>
