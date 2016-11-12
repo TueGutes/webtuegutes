842ce7453e2958c94ad565f08fd955eab24e1974
Klausio klausio <Klausio klausio>
2016-11-12 22:04:52 +0100

    deeds bearbeitet

--- deeds.php
+++ deeds.php
@@ -22,6 +22,7 @@ if(!isset($_GET['page']) /* || db_getGuteTatenAnzahl() >=  */) $_GET['page'] = 1
 <br><hr>
 </div>
 <br> 
+	
 		<?php
 		
 			/*$intZahl=0;
@@ -31,7 +32,8 @@ if(!isset($_GET['page']) /* || db_getGuteTatenAnzahl() >=  */) $_GET['page'] = 1
 			//$allDeedsCount = db_connector blabla
 			//$neededPages = $allDeedsCount/10;
 			
-			$arr = db_getGuteTatenForList(10*($_GET['page']-1), 10);
+			$tatenProSeite=10; //anzahl der tataen die pro seite angezeigt werden
+			$arr = db_getGuteTatenForList($tatenProSeite*($_GET['page']-1), $tatenProSeite);
 			$maxZeichenFürDieKurzbeschreibung = 150;
 
 			for($i = 0; $i < sizeof($arr); $i++){
@@ -46,14 +48,47 @@ if(!isset($_GET['page']) /* || db_getGuteTatenAnzahl() >=  */) $_GET['page'] = 1
 			}
 			
 		?>
-<br><hr>	
+<br><hr>
+	
+<?php
+
+$anzahlAllerTaten=77; //db_getGuteTatenAnzahl(); //zu test zwecken wurde hier die zahl 77 eingefügt 
+$aktuelleSeite=1;// dieser wert miss mit eins beginnen und die seiten zahl annehmen die der user anklickt zB nächste seite
+
+
+if($anzahlAllerTaten%$tatenProSeite==0){
+	$seitenanzahl=$anzahlAllerTaten/$tatenProSeite;// berechnet die benötigte seiten anzahl
+}else{
+	$seitenanzahl=$anzahlAllerTaten/$tatenProSeite+1; // berechnet die benötigte seiten anzahl
+}
+$maxSeitenLinks=7 ; // die menge der gleichzeitig angezeigten seiten zB bei 7 -> 3 4 5 6 7 8 9 wir befinden uns auf seite 6
+			
+			function zahlenausgabe($von,$bis){//schreibt die seiten zahlen
+				for($i=$von;$i<=$bis;$i++){
+					//der link get auf eine falsche seite er muss diese seite nochmal aufgerufen werden nur mit der pasenden seiten nummer
+					echo /*'<a href="./deeds?page=' . ($1) . '">*/'<div>'. $i .'</div></a>';// die zahlen stehen untereinander !!! fehler die sollen natuerlich nebeneinander stehen
+				}
+			}
+			
+			if($seitenanzahl>$maxSeitenLinks){
+				if($aktuelleSeite<($maxSeitenLinks/2)+1){
+					zahlenausgabe(1,$maxSeitenLinks);
+				}else{
+					printf ("<a href='./deeds'><div> Erste Seite </div>");// der link geht jetzt
+					zahlenausgabe($aktuelleSeite-3,$aktuelleSeite+3);
+				}
+				printf ("<a href='./deeds_details?id=" . $i . "' class='deedAnchor'><div> Letzte Seite </div>");// die links sind falsch kp wie ich an die atribute kommen die die seiten hoch zaehlen
+			}else{
+				zahlenausgabe(1,$seitenanzahl);
+			}
+?>	
 
 </form>
 
 <!--Zurück / Weiter Buttons-->
 <?php
 	$vor = $_GET['page'] > 1;
-	$nach = db_getGuteTatenForList(10*($_GET['page']), 10); //Sobald Deeds nicht mehr doppelt ausgegeben werden -> Mit Anzahl prüfen!
+	$nach = db_getGuteTatenForList($tatenProSeite*($_GET['page']), $tatenProSeite); //Sobald Deeds nicht mehr doppelt ausgegeben werden -> Mit Anzahl prüfen!
 	if ($vor) echo '<a href="./deeds?page=' . ($_GET['page']-1) . '"><input type="button" value="Zurück"></a>';
 	if ($vor && $nach) echo '&nbsp';
 	if ($nach) echo '<a href="./deeds?page=' . ($_GET['page']+1) . '"><input type="button" value="Weiter"></a>';
