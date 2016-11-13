1aae6c62609ddf516b3e4a4caddef7b4627fe7c7
nolting <nick.nolting@stud.hs-hannover.de>
2016-11-13 12:37:55 +0100

    Task 3214 (Gute-Taten-Liste mit Seitenübersicht) mergen und kleinere Bugfixes)

--- deeds.php
+++ deeds.php
@@ -41,49 +41,60 @@ if(!isset($_GET['page']) /* || db_getGuteTatenAnzahl() >=  */) $_GET['page'] = 1
 				echo "<a href='./deeds_details?id=" . $arr[$i]->idGuteTat . "' class='deedAnchor'><div class='deed" . ($arr[$i]->status == "geschlossen" ? " closed" : "") . "'>";
 					echo "<div class='name'><h4>" . $arr[$i]->name . "</h4></div><div class='category'>" . $arr[$i]->category . "</div>";
 					echo "<br><br><br><br><div class='description'>" . (strlen($arr[$i]->description) > $maxZeichenFürDieKurzbeschreibung ? substr($arr[$i]->description, 0, $maxZeichenFürDieKurzbeschreibung) . "...<br>mehr" : $arr[$i]->description) . "</div>";
-					echo "<div class='address'>" . $arr[$i]->street .  " ,  " . $arr[$i]->housenumber . " ,  " . $arr[$i]->postalcode . "</div>";
-					echo "<div>" . (is_numeric($arr[$i]->countHelper) ? "Anzahl der Helfer: " . $arr[$i]->countHelper : '') ."</div><div class='trustLevel'>Das Mindest Vertraunslevel betraegt " . $arr[$i]->idTrust . "</div>";
+					echo "<div class='address'>" . $arr[$i]->street .  "  " . $arr[$i]->housenumber . "<br>" . $arr[$i]->postalcode . ' / ' . $arr[$i]->place . "</div>";
+					echo "<div>" . (is_numeric($arr[$i]->countHelper) ? "Anzahl der Helfer: " . $arr[$i]->countHelper : '') ."</div><div class='trustLevel'>Minimaler Vertrauenslevel: " . $arr[$i]->idTrust . " (" . $arr[$i]->trustleveldescription . ")</div>";
 					echo "<div>" . $arr[$i]->organization . "</div>";
 				echo "</div></a>";
-				echo "<br><br><hr><br><br>";
+				echo "<br><br><hr><br>";
 			}
 			
 		?>
-<br><hr>
 	
-<?php
+		<?php
 
-$anzahlAllerTaten=77; //db_getGuteTatenAnzahl(); //zu test zwecken wurde hier die zahl 77 eingefügt 
-$aktuelleSeite=1;// dieser wert miss mit eins beginnen und die seiten zahl annehmen die der user anklickt zB nächste seite
+		$anzahlAllerTaten=db_getGuteTatenAnzahl();
+		$aktuelleSeite=$_GET['page'];
+		$letzteseite=intval(db_getGuteTatenAnzahl() / $tatenProSeite);
+		if ($letzteseite * $tatenProSeite < db_getGuteTatenAnzahl()) $letzteseite++;
 
+		if($anzahlAllerTaten%$tatenProSeite==0){
+			$seitenanzahl=$anzahlAllerTaten/$tatenProSeite;// berechnet die benötigte seiten anzahl
+		}else{
+			$seitenanzahl=$anzahlAllerTaten/$tatenProSeite+1; // berechnet die benötigte seiten anzahl
+		}
+		$maxSeitenLinks=7 ; // die menge der gleichzeitig angezeigten seiten zB bei 7 -> 3 4 5 6 7 8 9 wir befinden uns auf seite 6
+					
+					function zahlenausgabe($von,$bis, $letzteseite){//schreibt die seiten zahlen
+						for($i=$von;$i<=$bis;$i++){
+							//der link get auf eine falsche seite er muss diese seite nochmal aufgerufen werden nur mit der pasenden seiten nummer
+							if ($i>0 && $i<=$letzteseite) echo '<a href="./deeds?page=' . $i . '">&nbsp'. $i .'&nbsp</a>';
+						}
+					}
+					
+					if($seitenanzahl>$maxSeitenLinks){
+						if($aktuelleSeite<($maxSeitenLinks/2)+1){
+							zahlenausgabe(1,$aktuelleSeite-1, $letzteseite);
+							echo '&nbsp' . $aktuelleSeite . '&nbsp';
+							zahlenausgabe($aktuelleSeite+1,$maxSeitenLinks, $letzteseite);
+							if ($letzteseite > $maxSeitenLinks) printf ('<a href="./deeds?page=' . $letzteseite . '"> ... ' . $letzteseite . '</a>');
+						} else if ($aktuelleSeite > $letzteseite- ($maxSeitenLinks/2)-1) {
+							if ($letzteseite > $maxSeitenLinks) printf ("<a href='./deeds'>". '1 ... ' ."</a>");// der link geht jetzt
+							zahlenausgabe($letzteseite-$maxSeitenLinks,$aktuelleSeite-1, $letzteseite);
+							echo '&nbsp' . $aktuelleSeite . '&nbsp';
+							zahlenausgabe($aktuelleSeite+1,$letzteseite, $letzteseite);
+						}else{
+							printf ("<a href='./deeds'>". '1 ... ' ."</a>");// der link geht jetzt
+							zahlenausgabe($aktuelleSeite-intval($maxSeitenLinks/2),$aktuelleSeite-1,$letzteseite);
+							echo '&nbsp' . $aktuelleSeite . '&nbsp';
+							zahlenausgabe($aktuelleSeite+1,$aktuelleSeite+intval($maxSeitenLinks/2),$letzteseite);
+							printf ('<a href="./deeds?page=' . $letzteseite . '"> ... ' . $letzteseite . '</a>');
+						}
+					}else{
+						zahlenausgabe(1,$seitenanzahl, $letzteseite);
+					}
+		?>
 
-if($anzahlAllerTaten%$tatenProSeite==0){
-	$seitenanzahl=$anzahlAllerTaten/$tatenProSeite;// berechnet die benötigte seiten anzahl
-}else{
-	$seitenanzahl=$anzahlAllerTaten/$tatenProSeite+1; // berechnet die benötigte seiten anzahl
-}
-$maxSeitenLinks=7 ; // die menge der gleichzeitig angezeigten seiten zB bei 7 -> 3 4 5 6 7 8 9 wir befinden uns auf seite 6
-			
-			function zahlenausgabe($von,$bis){//schreibt die seiten zahlen
-				for($i=$von;$i<=$bis;$i++){
-					//der link get auf eine falsche seite er muss diese seite nochmal aufgerufen werden nur mit der pasenden seiten nummer
-					echo /*'<a href="./deeds?page=' . ($1) . '">*/'<div>'. $i .'</div></a>';// die zahlen stehen untereinander !!! fehler die sollen natuerlich nebeneinander stehen
-				}
-			}
-			
-			if($seitenanzahl>$maxSeitenLinks){
-				if($aktuelleSeite<($maxSeitenLinks/2)+1){
-					zahlenausgabe(1,$maxSeitenLinks);
-				}else{
-					printf ("<a href='./deeds'><div> Erste Seite </div>");// der link geht jetzt
-					zahlenausgabe($aktuelleSeite-3,$aktuelleSeite+3);
-				}
-				printf ("<a href='./deeds_details?id=" . $i . "' class='deedAnchor'><div> Letzte Seite </div>");// die links sind falsch kp wie ich an die atribute kommen die die seiten hoch zaehlen
-			}else{
-				zahlenausgabe(1,$seitenanzahl);
-			}
-?>	
-
+<br><br><hr>
 </form>
 
 <!--Zurück / Weiter Buttons-->
