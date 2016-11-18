2baf569059ef6e057b05781412de4acb5bea5bd1
blecha <andreas.blech@stud.hs-hannover.de>
2016-11-18 08:40:04 +0100

    Noch eine unerlaubte Änderung

--- includes/db_connector.php
+++ includes/db_connector.php
@@ -3,7 +3,7 @@
 * @author Timm Romanik <timm.romanik@stud.hs-hannover.de
 */
 
-//Noch eine Teständerung, um die Permissions zu testen
+//Noch 123 eine Teständerung, um die Permissions zu testen
 
 //Definition der Datenbankverbindung
 DEFINE('DB_USER','tueGutes');
