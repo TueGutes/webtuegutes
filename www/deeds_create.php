6b3863f820f1f76e2ca94682be31bd3de6f407ad
wesone <mail@wesone.de>
2016-12-07 11:17:11 +0100

    deeds create zeit und datum trennen

--- www/deeds_create.php
+++ www/deeds_create.php
@@ -200,18 +200,18 @@ if(($Seite==4 ||$Seite==5)&& $button=='weiter' ){
 				$end_dh = (new DateHandler())->set($enddate);
 				if($BROWSER_NAME === 'Firefox' || $BROWSER_NAME === 'Internet Explorer')
 				{
-					echo '<input type="text" name="startdate" value="' . $start_dh ? $start_dh->get('d.m.Y') : '' . '" placeholder="Startzeitpunkt (dd.mm.yyyy HH:MM)" required /><br>';
+					echo '<input type="text" name="startdate" value="' . ($start_dh !== false ? $start_dh->get('d.m.Y') : '') . '" placeholder="Startzeitpunkt (dd.mm.yyyy HH:MM)" required /><br>';
 					echo '<input type="time" name="starttime" placeholder="HH:MM">';
 					
-					echo '<input type="text" name="enddate" value="' . $end_dh ? $end_dh->get('d.m.Y') : '' . '" placeholder="Endzeitpunkt (dd.mm.yyyy HH:MM)" required />';
+					echo '<input type="text" name="enddate" value="' . ($end_dh !== false ? $end_dh->get('d.m.Y') : '') . '" placeholder="Endzeitpunkt (dd.mm.yyyy HH:MM)" required />';
 					echo '<input type="time" name="starttime" placeholder="HH:MM">';
 				}
 				else
 				{
-					echo '<input type="date" class="dateSelector" name="startdate" value="' . $start_dh ? $start_dh->get('Y-m-d') : '' . '" placeholder="Startzeitpunkt (dd.mm.yyyy HH:MM)" required /><br>';
+					echo '<input type="date" class="dateSelector" name="startdate" value="' . ($start_dh !== false ? $start_dh->get('Y-m-d') : '') . '" placeholder="Startzeitpunkt (dd.mm.yyyy HH:MM)" required /><br>';
 					echo '<input type="time" name="starttime" placeholder="HH:MM">';
 					
-					echo '<input type="date" class="dateSelector" name="enddate" value="' . $end_dh ? $end_dh->get('Y-m-d') : '' . '" placeholder="Endzeitpunkt (dd.mm.yyyy HH:MM)" required />';
+					echo '<input type="date" class="dateSelector" name="enddate" value="' . ($end_dh !== false ? $end_dh->get('Y-m-d') : '') . '" placeholder="Endzeitpunkt (dd.mm.yyyy HH:MM)" required />';
 					echo '<input type="time" name="starttime" placeholder="HH:MM">';
 				}
 
