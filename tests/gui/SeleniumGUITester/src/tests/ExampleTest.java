c7884648301356eb67a2c34dac65a06bafa946d1
Max Lindemann <max.lindemann@stud.hs-hannover.de>
2016-12-08 12:12:31 +0100

    Hinzufügen der Selenium Testumgebung

---
+++ tests/gui/SeleniumGUITester/src/tests/ExampleTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+
+import starter.GUITest;
+
+//Beispiel für eine TestKlasse
+public class ExampleTest extends GUITest{
+
+	@Override
+	public boolean doTest(WebDriver wd, String rootUrl) throws WebDriverException {
+		//Hier der Test-Code
+		return false;
+	}
+	
+
+	
+	
+
+
+}
