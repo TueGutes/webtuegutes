566851dd926a9b0bf13b8c28d2f78a7181f4ac1c
blecha <andreas.blech@stud.hs-hannover.de>
2016-12-16 15:45:19 +0100

    Datenschutzerklärung und Impressum eingebunden + Checkbox bei der Registrierung

--- www/terms.php
+++ www/terms.php
@@ -14,7 +14,7 @@ require './includes/_top.php';
 <h2><?php echo $wlang['terms_head']; ?></h2>
 
 <div>
-Unsere AGBs
+Unsere AGBs <!-- TODO: Sobald verfügbar in der registration.php bei der Checkbox einen Link auf die AGBs hinzufügen-->
 </div>
 
 <?php
