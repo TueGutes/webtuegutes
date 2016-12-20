70a300216a054626d51e235f3a3ae76ae40fc271
Timm Romanik <timm.romanik@stud.hs-hannover.de>
2016-12-20 14:38:42 +0100

    Nötige Daten der Kategorien schon standard mäßig in die DB überführen

--- db/migrations/20161130145612_add_category_table.php
+++ db/migrations/20161130145612_add_category_table.php
@@ -25,10 +25,43 @@ class AddCategoryTable extends AbstractMigration
      * Remember to call "create()" or "update()" and NOT "save()" when working
      * with the Table class.
      */
+    /**
     public function change()
     {
     	$category = $this->table('Categories');
     	$category->addColumn('categoryname','string',array('limit' => 128))
     			 ->create();
     }
+    **/
+    public function up(){
+        $category = $this->table('Categories');
+        $category->addColumn('categoryname','string',array('limit' => 128))
+                 ->create();
+
+        $rows = [
+            ['categoryname' => 'keine Angabe'],
+            ['categoryname' => 'Öffentliches'],
+            ['categoryname' => 'Mensch zu Mensch'],
+            ['categoryname' => 'Bildung'],
+            ['categoryname' => 'Längerfristig'],
+            ['categoryname' => 'Social Event'],
+            ['categoryname' => 'Haushalt']
+        ];
+
+        // this is a handy shortcut
+        $this->insert('Categories', $rows);
+    }
+
+    public function down(){
+        $this->execute('DELETE FROM Categories WHERE categoryname = "keine Angabe"');
+        $this->execute('DELETE FROM Categories WHERE categoryname = "Öffentliches"');
+        $this->execute('DELETE FROM Categories WHERE categoryname = "Mensch zu Mensch"');
+        $this->execute('DELETE FROM Categories WHERE categoryname = "Bildung"');
+        $this->execute('DELETE FROM Categories WHERE categoryname = "Längerfristig"');
+        $this->execute('DELETE FROM Categories WHERE categoryname = "Social Event"');
+        $this->execute('DELETE FROM Categories WHERE categoryname = "Haushalt"');
+
+        $this->dropTable('Categories');
+    }
 }
+
