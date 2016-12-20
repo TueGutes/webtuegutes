828edf18b6ff43cf9e2f2e017ff7bada30fb6e28
Timm Romanik <timm.romanik@stud.hs-hannover.de>
2016-12-20 14:18:16 +0100

    Nötige Daten werden für Trust und Usergroup direkt eingepflegt

--- db/migrations/20161120192439_add_trust_table.php
+++ db/migrations/20161120192439_add_trust_table.php
@@ -25,11 +25,61 @@ class AddTrustTable extends AbstractMigration
      * Remember to call "create()" or "update()" and NOT "save()" when working
      * with the Table class.
      */
+    /**
     public function change()
     {
         $trust = $this->table('Trust', array('id'=> false,'primary_key' => array('idTrust')));
         $trust->addColumn('idTrust','integer')
              ->addColumn('trustleveldescription','string',array('limit' => 45))
              ->create();
+    }
+    **/
+    public function up(){
+        $trust = $this->table('Trust', array('id'=> false,'primary_key' => array('idTrust')));
+        $trust->addColumn('idTrust','integer')
+             ->addColumn('trustleveldescription','string',array('limit' => 45))
+             ->create();
+
+        $rows = [
+            [
+                'idTrust' => 1,
+                'trustleveldescription' => 'Neuling'
+            ],
+            [
+                'idTrust' => 2,
+                'trustleveldescription' => 'Mitglied'
+            ],
+            [
+                'idTrust' => 3,
+                'trustleveldescription' => 'Stammmitglied'
+            ],
+            [
+                'idTrust' => 4,
+                'trustleveldescription' => 'Veteran'
+            ],
+            [
+                'idTrust' => 5,
+                'trustleveldescription' => 'GuteFreund'
+            ],
+            [
+                'idTrust' => 6,
+                'trustleveldescription' => 'Familienmitglied'
+            ],
+            [
+                'idTrust' => 7,
+                'trustleveldescription' => 'Seelenverwandte'
+            ]
+        ];
+
+        // this is a handy shortcut
+        $this->insert('Trust', $rows);
+    }
+
+    public function down(){
+
+
+        $this->execute('DELETE FROM Trust WHERE idTrust < 8');
+
+        $this->dropTable('Trust');
     }
 }
