7fc51fd8806ab49162c9f82990c82d5637529925
Timm Romanik <timm.romanik@stud.hs-hannover.de>
2016-12-21 15:30:13 +0100

    ss

--- www/deeds_bearbeiten.php
+++ www/deeds_bearbeiten.php
@@ -112,6 +112,7 @@ if(isset($_GET['id']))$idGuteTat = $_GET['id'];
 		$data=$_POST['von'];
 		$data2=$_POST['bis'];
 	if (!DateHandler::isValid($data)){
+		echo .$data;
 		echo 'Das Format von der Startzeit ist falsch.';
 	}else if (!DateHandler::isValid($data2)){
 		echo 'Das Format von der Endzeit ist falsch.';
